68ba244c05f7a45516c5fafc11ae82e6b36ed7e5
Andrew <andrewmarello@usf.edu>
2026-04-06 13:31:40 -0400

    In-Class Commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import TaskCard from '@/components/TaskCard';
+
+export default function HomePage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Task Manager</h1>
+
+      <TaskCard title="Buy milk" done={false} />
+      <TaskCard title="Write tests" done={true} />
+      <TaskCard title="Ship it" done={false} />
+    </main>
+  );
+}
